27b2ecde38f9663b7f4fac62c82d8e6bd740fe51
Al Mahmud <77141608+almahmud249@users.noreply.github.com>
2023-10-16 12:22:56 +0600

    submit btn text change

--- resources/views/Individual/deposit-form.blade.php
+++ resources/views/Individual/deposit-form.blade.php
@@ -9,7 +9,7 @@
             <input type="hidden" class="form-control" name="transaction_type" value="deposit" id="exampleFormControlInput1" placeholder="0.00">
             {{ session('error') }}
         </div>
-        <button type="submit" class="btn btn-primary">Primary</button>
+        <button type="submit" class="btn btn-primary">Submit</button>
     </form>
 
 @endsection
